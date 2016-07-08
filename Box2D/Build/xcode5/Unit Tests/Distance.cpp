4d696b64ed2e513499341c546684df07f488f5e7
louis-langholtz <lou_langholtz@me.com>
2016-07-08 16:18:34 -0600

    Adds unit test code.

--- Box2D/Build/xcode5/Unit Tests/Distance.cpp
+++ Box2D/Build/xcode5/Unit Tests/Distance.cpp
@@ -262,3 +262,86 @@ TEST(Distance, EdgeCircleTouching)
 	EXPECT_EQ(true, cache.IsMetricSet());
 	EXPECT_EQ(cache.GetMetric(), float_t{4});
 }
+
+TEST(Distance, HorEdgeSquareTouching)
+{
+	const auto pos1 = Vec2{1, 1};
+	const auto pos2 = Vec2{1, 3};
+	const auto pos3 = Vec2{3, 3};
+	const auto pos4 = Vec2{3, 1};
+	const Vec2 square[] = {pos1, pos2, pos3, pos4};
+	DistanceProxy dp1{float_t(0.5), square, 4};
+
+	const auto pos5 = Vec2{-2, 0};
+	const auto pos6 = Vec2{6, 0};
+	DistanceProxy dp2{float_t(0.5), pos5, pos6};
+	
+	SimplexCache cache;
+	Transformation xf1 = Transform_identity;
+	Transformation xf2 = Transform_identity;
+
+	const auto output = Distance(cache, dp1, xf1, dp2, xf2);
+	
+	EXPECT_EQ(output.witnessPoints.a.x, float_t{1});
+	EXPECT_EQ(output.witnessPoints.a.y, float_t{1});
+	
+	EXPECT_EQ(output.witnessPoints.b.x, float_t{1});
+	EXPECT_EQ(output.witnessPoints.b.y, float_t{0});
+	
+	EXPECT_EQ(decltype(output.iterations){2}, output.iterations);
+	
+	EXPECT_EQ(cache.GetCount(), decltype(cache.GetCount()){2});
+	
+	const auto ip0 = cache.GetIndexPair(0);
+	EXPECT_EQ(ip0.a, IndexPair::size_type{0});
+	EXPECT_EQ(ip0.b, IndexPair::size_type{0});
+	
+	const auto ip1 = cache.GetIndexPair(1);
+	EXPECT_EQ(ip1.a, IndexPair::size_type{0});
+	EXPECT_EQ(ip1.b, IndexPair::size_type{1});
+	
+	EXPECT_EQ(true, cache.IsMetricSet());
+	EXPECT_EQ(cache.GetMetric(), float_t{8});
+}
+
+TEST(Distance, VerEdgeSquareTouching)
+{
+	const auto pos1 = Vec2{1, 1};
+	const auto pos2 = Vec2{1, 3};
+	const auto pos3 = Vec2{3, 3};
+	const auto pos4 = Vec2{3, 1};
+	const Vec2 square[] = {pos1, pos2, pos3, pos4};
+	DistanceProxy dp1{float_t(0.5), square, 4};
+	
+	const auto pos5 = Vec2{4, -2};
+	const auto pos6 = Vec2{4, 6};
+	DistanceProxy dp2{float_t(0.5), pos5, pos6};
+	
+	SimplexCache cache;
+	Transformation xf1 = Transform_identity;
+	Transformation xf2 = Transform_identity;
+	
+	const auto output = Distance(cache, dp1, xf1, dp2, xf2);
+	
+	EXPECT_EQ(Sqrt(LengthSquared(output.witnessPoints.a - output.witnessPoints.b)), float_t(1));
+	EXPECT_EQ(output.witnessPoints.a.x, float_t{3});
+	EXPECT_EQ(output.witnessPoints.a.y, float_t{2});
+	
+	EXPECT_EQ(output.witnessPoints.b.x, float_t{4});
+	EXPECT_EQ(output.witnessPoints.b.y, float_t{2});
+	
+	EXPECT_EQ(decltype(output.iterations){3}, output.iterations);
+	
+	EXPECT_EQ(cache.GetCount(), decltype(cache.GetCount()){2});
+	
+	const auto ip0 = cache.GetIndexPair(0);
+	EXPECT_EQ(ip0.a, IndexPair::size_type{2});
+	EXPECT_EQ(ip0.b, IndexPair::size_type{0});
+	
+	const auto ip1 = cache.GetIndexPair(1);
+	EXPECT_EQ(ip1.a, IndexPair::size_type{3});
+	EXPECT_EQ(ip1.b, IndexPair::size_type{1});
+	
+	EXPECT_EQ(true, cache.IsMetricSet());
+	EXPECT_EQ(cache.GetMetric(), float_t{10});
+}
