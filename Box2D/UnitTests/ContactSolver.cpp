e994a1525a235eac85db0b649ae1ac06ff80fbc7
louis-langholtz <lou_langholtz@me.com>
2016-10-07 15:04:15 -0600

    Updates unit test code for the Solve function.

--- Box2D/UnitTests/ContactSolver.cpp
+++ Box2D/UnitTests/ContactSolver.cpp
@@ -38,7 +38,7 @@ TEST(ContactSolver, ZeroCountInit)
 	EXPECT_TRUE(solver.SolveTOIPositionConstraints(0, 0));
 }
 
-TEST(ContactSolver, SolveTouching)
+TEST(ContactSolver, SolveHorizontallyTouchingDoesntMove)
 {
 	const auto old_pA = Position{Vec2{-2, 0}, 0};
 	const auto old_pB = Position{Vec2{+2, 0}, 0};
@@ -67,6 +67,40 @@ TEST(ContactSolver, SolveTouching)
 	EXPECT_EQ(old_pA.c.y, solution.pos_a.c.y);
 	EXPECT_EQ(old_pA.a, solution.pos_a.a);
 
+	EXPECT_EQ(old_pB.c.x, solution.pos_b.c.x);
+	EXPECT_EQ(old_pB.c.y, solution.pos_b.c.y);
+	EXPECT_EQ(old_pB.a, solution.pos_b.a);
+}
+
+TEST(ContactSolver, SolveVerticallyTouchingDoesntMove)
+{
+	const auto old_pA = Position{Vec2{0, -2}, 0};
+	const auto old_pB = Position{Vec2{0, +2}, 0};
+	
+	const auto dim = float_t(2);
+	const auto shape = PolygonShape(dim, dim);
+	const auto xfmA = Transformation(old_pA.c, Rot{old_pA.a});
+	const auto xfmB = Transformation(old_pB.c, Rot{old_pB.a});
+	const auto manifold = CollideShapes(shape, xfmA, shape, xfmB);
+	ASSERT_EQ(manifold.GetType(), Manifold::e_faceA);
+	ASSERT_EQ(manifold.GetPointCount(), 2);
+	
+	const auto indexA = PositionConstraint::BodyData::index_type{0};
+	const auto indexB = PositionConstraint::BodyData::index_type{0};
+	const auto lcA = Vec2{0, 0};
+	const auto lcB = Vec2{0, 0};
+	const auto bA = PositionConstraint::BodyData{indexA, float_t(1), float_t(1), lcA};
+	const auto bB = PositionConstraint::BodyData{indexB, float_t(1), float_t(1), lcB};
+	const auto pc = PositionConstraint{manifold, bA, 0, bB, 0};
+	
+	const auto solution = Solve(pc, old_pA, old_pB, Baumgarte, 0, MaxLinearCorrection);
+	
+	EXPECT_EQ(solution.min_separation, 0);
+	
+	EXPECT_EQ(old_pA.c.x, solution.pos_a.c.x);
+	EXPECT_EQ(old_pA.c.y, solution.pos_a.c.y);
+	EXPECT_EQ(old_pA.a, solution.pos_a.a);
+	
 	EXPECT_EQ(old_pB.c.x, solution.pos_b.c.x);
 	EXPECT_EQ(old_pB.c.y, solution.pos_b.c.y);
 	EXPECT_EQ(old_pB.a, solution.pos_b.a);
@@ -106,6 +140,62 @@ TEST(ContactSolver, SolveOverlappingZeroRateDoesntMove)
 	EXPECT_EQ(old_pB.a, solution.pos_b.a);
 }
 
+TEST(ContactSolver, SolveHorizontallyOverlapping)
+{
+	const auto old_pA = Position{Vec2{-1, 0}, 0};
+	const auto old_pB = Position{Vec2{+1, 0}, 0};
+	
+	const auto dim = float_t(2);
+	const auto shape = PolygonShape(dim, dim);
+	const auto xfmA = Transformation(old_pA.c, Rot{old_pA.a});
+	const auto xfmB = Transformation(old_pB.c, Rot{old_pB.a});
+	const auto manifold = CollideShapes(shape, xfmA, shape, xfmB);
+	ASSERT_EQ(manifold.GetType(), Manifold::e_faceA);
+	ASSERT_EQ(manifold.GetLocalNormal().x, float_t(1));
+	ASSERT_EQ(manifold.GetLocalNormal().y, float_t(0));
+	ASSERT_EQ(manifold.GetLocalPoint().x, float_t(2));
+	ASSERT_EQ(manifold.GetLocalPoint().y, float_t(0));
+	ASSERT_EQ(manifold.GetPointCount(), 2);
+	ASSERT_EQ(manifold.GetPoint(0).localPoint.x, float_t(-2));
+	ASSERT_EQ(manifold.GetPoint(0).localPoint.y, float_t(+2));
+	ASSERT_EQ(manifold.GetPoint(1).localPoint.x, float_t(-2));
+	ASSERT_EQ(manifold.GetPoint(1).localPoint.y, float_t(-2));
+	
+	const auto indexA = PositionConstraint::BodyData::index_type{0};
+	const auto indexB = PositionConstraint::BodyData::index_type{0};
+	const auto lcA = Vec2{0, 0};
+	const auto lcB = Vec2{0, 0};
+	const auto bA = PositionConstraint::BodyData{indexA, float_t(1), float_t(1), lcA};
+	const auto bB = PositionConstraint::BodyData{indexB, float_t(1), float_t(1), lcB};
+	const auto pc = PositionConstraint{manifold, bA, 0, bB, 0};
+	
+	const auto solution = Solve(pc, old_pA, old_pB, Baumgarte, 0, MaxLinearCorrection);
+	
+	EXPECT_FLOAT_EQ(solution.min_separation, float_t(-2.002398));
+		
+	// object a moves down left and rotates counter-clockwise
+	EXPECT_LT(solution.pos_a.c.x, old_pA.c.x);
+	EXPECT_LT(solution.pos_a.c.y, old_pA.c.y);
+	EXPECT_LT(solution.pos_a.a, old_pA.a);
+
+	{
+		// confirm object a moves more in x direction than in y direction.
+		const auto mov_a = solution.pos_a - old_pA;	
+		EXPECT_GT(Abs(mov_a.c.x), Abs(mov_a.c.y));
+	}
+	
+	// object b moves up right and rotates clockwise.
+	EXPECT_GT(solution.pos_b.c.x, old_pB.c.x);
+	EXPECT_GT(solution.pos_b.c.y, old_pB.c.y);
+	EXPECT_GT(solution.pos_b.a, old_pB.a);
+
+	{
+		// confirm object a moves more in x direction than in y direction.
+		const auto mov_b = solution.pos_b - old_pB;
+		EXPECT_GT(Abs(mov_b.c.x), Abs(mov_b.c.y));
+	}
+}
+
 TEST(ContactSolver, SolvePerfectlyOverlappingSquares)
 {
 	const auto dim = float_t(2);
@@ -123,20 +213,22 @@ TEST(ContactSolver, SolvePerfectlyOverlappingSquares)
 	const auto bA = PositionConstraint::BodyData{indexA, float_t(1), float_t(1), lcA};
 	const auto bB = PositionConstraint::BodyData{indexB, float_t(1), float_t(1), lcB};
 	const auto pc = PositionConstraint{manifold, bA, GetRadius(shape), bB, GetRadius(shape)};
+	const auto max_sep = -LinearSlop;
 
 	const auto old_pA = Position{Vec2{0, 0}, 0};
 	const auto old_pB = Position{Vec2{0, 0}, 0};
 
-	const auto max_sep = -LinearSlop;
 	const auto solution = Solve(pc, old_pA, old_pB, Baumgarte, max_sep, MaxLinearCorrection);
 	
 	EXPECT_LT(solution.min_separation, max_sep);
 	
-	EXPECT_EQ(old_pA.c.x, solution.pos_a.c.x);
-	EXPECT_EQ(old_pA.c.y, solution.pos_a.c.y);
-	EXPECT_EQ(old_pA.a, solution.pos_a.a);
+	// object a moves down left and rotates counter-clockwise
+	EXPECT_LT(solution.pos_a.c.x, old_pA.c.x);
+	EXPECT_LT(solution.pos_a.c.y, old_pA.c.y);
+	EXPECT_LT(solution.pos_a.a, old_pA.a);
 	
-	EXPECT_EQ(old_pB.c.x, solution.pos_b.c.x);
-	EXPECT_EQ(old_pB.c.y, solution.pos_b.c.y);
-	EXPECT_EQ(old_pB.a, solution.pos_b.a);
+	// object b moves up right and rotates clockwise.
+	EXPECT_GT(solution.pos_b.c.x, old_pB.c.x);
+	EXPECT_GT(solution.pos_b.c.y, old_pB.c.y);
+	EXPECT_GT(solution.pos_b.a, old_pB.a);
 }
