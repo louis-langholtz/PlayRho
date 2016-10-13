4fd7340e895e9cf14514ea133c0529d4ced1bc8e
louis-langholtz <lou_langholtz@me.com>
2016-10-13 10:41:12 -0600

    updates unit test code to fix test to actually test as originally intended.

--- Box2D/UnitTests/Collision.cpp
+++ Box2D/UnitTests/Collision.cpp
@@ -241,8 +241,10 @@ TEST(Collision, SquareCornerUnderSquareFace)
 	ASSERT_EQ(shape.GetVertex(3).x, float_t(-2)); // left
 	ASSERT_EQ(shape.GetVertex(3).y, float_t(-2)); // bottom
 	
-	const auto xfm0 = Transformation(Vec2{0, -1}, Rot{DegreesToRadians(45)}); // bottom
-	const auto xfm1 = Transformation(Vec2{0, +1}, Rot{0}); // top
+	const auto rot0 = Rot{DegreesToRadians(45)};
+	const auto rot1 = Rot{0};
+	const auto xfm0 = Transformation(Vec2{0, -2}, rot0); // bottom
+	const auto xfm1 = Transformation(Vec2{0, +2}, rot1); // top
 	const auto manifold = CollideShapes(shape, xfm0, shape, xfm1);
 	
 	EXPECT_EQ(manifold.GetType(), Manifold::e_faceB);
@@ -253,7 +255,7 @@ TEST(Collision, SquareCornerUnderSquareFace)
 	EXPECT_EQ(manifold.GetLocalNormal().x, float_t(0));
 	EXPECT_EQ(manifold.GetLocalNormal().y, float_t(-1));
 	
-	EXPECT_EQ(manifold.GetPointCount(), Manifold::size_type(2));
+	EXPECT_EQ(manifold.GetPointCount(), Manifold::size_type(1));
 	
 	ASSERT_GT(manifold.GetPointCount(), Manifold::size_type(0));
 	EXPECT_FLOAT_EQ(manifold.GetPoint(0).localPoint.x, float_t(+2)); // left
@@ -265,15 +267,21 @@ TEST(Collision, SquareCornerUnderSquareFace)
 	EXPECT_EQ(manifold.GetPoint(0).contactFeature.typeB, ContactFeature::e_face);
 	EXPECT_EQ(manifold.GetPoint(0).contactFeature.indexB, 3);
 	
-	ASSERT_GT(manifold.GetPointCount(), Manifold::size_type(1));
-	EXPECT_FLOAT_EQ(manifold.GetPoint(1).localPoint.x, float_t(+2)); // right
-	EXPECT_FLOAT_EQ(manifold.GetPoint(1).localPoint.y, float_t(-0.8289929)); // bottom
-	EXPECT_FLOAT_EQ(manifold.GetPoint(1).normalImpulse, float_t(0));
-	EXPECT_FLOAT_EQ(manifold.GetPoint(1).tangentImpulse, float_t(0));
-	EXPECT_EQ(manifold.GetPoint(1).contactFeature.typeA, ContactFeature::e_face);
-	EXPECT_EQ(manifold.GetPoint(1).contactFeature.indexA, 0);
-	EXPECT_EQ(manifold.GetPoint(1).contactFeature.typeB, ContactFeature::e_vertex);
-	EXPECT_EQ(manifold.GetPoint(1).contactFeature.indexB, 0);
+	// Also check things in terms of world coordinates...
+	const auto world_manifold = GetWorldManifold(manifold, xfm0, float_t(0), xfm1, float_t(0));
+	EXPECT_EQ(world_manifold.GetPointCount(), manifold.GetPointCount());
+	
+	EXPECT_FLOAT_EQ(world_manifold.GetNormal().x, float_t(0));
+	EXPECT_FLOAT_EQ(world_manifold.GetNormal().y, float_t(1));
+	
+	const auto corner_point = Rotate(Vec2{dim, dim}, rot0) + xfm0.p;
+	EXPECT_FLOAT_EQ(corner_point.x, float_t(0));
+	EXPECT_FLOAT_EQ(corner_point.y, float_t(0.82842684));
+	
+	ASSERT_GT(world_manifold.GetPointCount(), Manifold::size_type(0));
+	EXPECT_FLOAT_EQ(world_manifold.GetPoint(0).x, corner_point.x / 2);
+	EXPECT_FLOAT_EQ(world_manifold.GetPoint(0).y, corner_point.y / 2);
+	EXPECT_FLOAT_EQ(world_manifold.GetSeparation(0), -corner_point.y);
 }
 
 TEST(Collision, HorizontalOverlappingRects1)
