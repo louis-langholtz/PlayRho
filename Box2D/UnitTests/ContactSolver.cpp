d0dcb654d43d639c7d25ff860885a0a5481a8604
louis-langholtz <lou_langholtz@me.com>
2016-10-18 10:55:20 -0600

    Updates unit test names and adds new test that succeeds as desired.

--- Box2D/UnitTests/ContactSolver.cpp
+++ Box2D/UnitTests/ContactSolver.cpp
@@ -24,7 +24,7 @@
 
 using namespace box2d;
 
-TEST(ContactSolver, SolveHorizontallyTouchingDoesntMove)
+TEST(ContactSolver, SolvePosConstraintsForHorTouchingDoesntMove)
 {
 	const auto old_pA = Position{Vec2{-2, 0}, 0};
 	const auto old_pB = Position{Vec2{+2, 0}, 0};
@@ -58,7 +58,7 @@ TEST(ContactSolver, SolveHorizontallyTouchingDoesntMove)
 	EXPECT_EQ(old_pB.a, solution.pos_b.a);
 }
 
-TEST(ContactSolver, SolveVerticallyTouchingDoesntMove)
+TEST(ContactSolver, SolvePosConstraintsForVerTouchingDoesntMove)
 {
 	const auto old_pA = Position{Vec2{0, -2}, 0};
 	const auto old_pB = Position{Vec2{0, +2}, 0};
@@ -92,7 +92,7 @@ TEST(ContactSolver, SolveVerticallyTouchingDoesntMove)
 	EXPECT_EQ(old_pB.a, solution.pos_b.a);
 }
 
-TEST(ContactSolver, SolveOverlappingZeroRateDoesntMove)
+TEST(ContactSolver, SolvePosConstraintsForOverlappingZeroRateDoesntMove)
 {
 	const auto dim = float_t(2);
 	const auto shape = PolygonShape(dim, dim);
@@ -126,7 +126,7 @@ TEST(ContactSolver, SolveOverlappingZeroRateDoesntMove)
 	EXPECT_EQ(old_pB.a, solution.pos_b.a);
 }
 
-TEST(ContactSolver, SolveHorizontallyOverlappingMovesHorizontallyOnly)
+TEST(ContactSolver, SolvePosConstraintsForHorOverlappingMovesHorOnly)
 {
 	const auto ctr_x = float_t(100);
 	const auto old_pA = Position{{ctr_x - 1, 0}, 0};
@@ -183,9 +183,11 @@ TEST(ContactSolver, SolveHorizontallyOverlappingMovesHorizontallyOnly)
 	}
 }
 
-TEST(ContactSolver, SolveVerticallyOverlappingMovesVerticallyOnly)
+TEST(ContactSolver, SolvePosConstraintsForVerOverlappingMovesVerOnly1)
 {
 	const auto ctr_y = float_t(100);
+	
+	// square A is below square B
 	const auto old_pA = Position{{0, ctr_y - 1}, 0};
 	const auto old_pB = Position{{0, ctr_y + 1}, 0};
 	
@@ -240,7 +242,66 @@ TEST(ContactSolver, SolveVerticallyOverlappingMovesVerticallyOnly)
 	}
 }
 
-TEST(ContactSolver, SolvePerfectlyOverlappingSquares)
+TEST(ContactSolver, SolvePosConstraintsForVerOverlappingMovesVerOnly2)
+{
+	const auto ctr_y = float_t(100);
+
+	// square A is above square B
+	const auto old_pA = Position{{0, ctr_y + 1}, 0};
+	const auto old_pB = Position{{0, ctr_y - 1}, 0};
+	
+	const auto dim = float_t(2);
+	const auto shape = PolygonShape(dim, dim);
+	const auto xfmA = Transformation(old_pA.c, Rot{old_pA.a});
+	const auto xfmB = Transformation(old_pB.c, Rot{old_pB.a});
+	const auto manifold = CollideShapes(shape, xfmA, shape, xfmB);
+	ASSERT_EQ(manifold.GetType(), Manifold::e_faceA);
+	ASSERT_EQ(manifold.GetLocalNormal().x, float_t(0));
+	ASSERT_EQ(manifold.GetLocalNormal().y, float_t(-1));
+	ASSERT_EQ(manifold.GetLocalPoint().x, float_t(0));
+	ASSERT_EQ(manifold.GetLocalPoint().y, float_t(-2));
+	ASSERT_EQ(manifold.GetPointCount(), 2);
+	ASSERT_EQ(manifold.GetPoint(0).localPoint.x, float_t(+2));
+	ASSERT_EQ(manifold.GetPoint(0).localPoint.y, float_t(+2));
+	ASSERT_EQ(manifold.GetPoint(1).localPoint.x, float_t(-2));
+	ASSERT_EQ(manifold.GetPoint(1).localPoint.y, float_t(+2));
+	
+	const auto indexA = PositionConstraint::BodyData::index_type{0};
+	const auto indexB = PositionConstraint::BodyData::index_type{0};
+	const auto lcA = Vec2{0, 0};
+	const auto lcB = Vec2{0, 0};
+	const auto bA = PositionConstraint::BodyData{indexA, float_t(1), float_t(1), lcA};
+	const auto bB = PositionConstraint::BodyData{indexB, float_t(1), float_t(1), lcB};
+	const auto pc = PositionConstraint{manifold, bA, 0, bB, 0};
+	
+	const auto solution = Solve(pc, old_pA, old_pB, Baumgarte, 0, MaxLinearCorrection);
+	
+	EXPECT_FLOAT_EQ(solution.min_separation, float_t(-2)); // -2.002398
+	
+	// square A just moves up only
+	EXPECT_EQ(solution.pos_a.c.x, old_pA.c.x);
+	EXPECT_GT(solution.pos_a.c.y, old_pA.c.y);
+	EXPECT_EQ(solution.pos_a.a, old_pA.a);
+	
+	{
+		// confirm object a moves more in x direction than in y direction.
+		const auto mov_a = solution.pos_a - old_pA;	
+		EXPECT_LT(Abs(mov_a.c.x), Abs(mov_a.c.y));
+	}
+	
+	// square B just moves down only
+	EXPECT_EQ(solution.pos_b.c.x, old_pB.c.x);
+	EXPECT_LT(solution.pos_b.c.y, old_pB.c.y);
+	EXPECT_EQ(solution.pos_b.a, old_pB.a);
+	
+	{
+		// confirm object a moves more in x direction than in y direction.
+		const auto mov_b = solution.pos_b - old_pB;
+		EXPECT_LT(Abs(mov_b.c.x), Abs(mov_b.c.y));
+	}
+}
+
+TEST(ContactSolver, SolvePosConstraintsForPerfectlyOverlappingSquares)
 {
 	const auto dim = float_t(2);
 	const auto shape = PolygonShape(dim, dim);
