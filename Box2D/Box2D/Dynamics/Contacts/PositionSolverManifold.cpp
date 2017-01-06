1ddc0356d1bd043d9adba2e8ac050ec92ca16f0c
louis-langholtz <lou_langholtz@me.com>
2017-01-06 15:13:03 -0700

    Switches private functions from being static to being within an unnamed namespace. Renames some variables in the interest of avoiding line breaks within function calls.

--- Box2D/Box2D/Dynamics/Contacts/PositionSolverManifold.cpp
+++ Box2D/Box2D/Dynamics/Contacts/PositionSolverManifold.cpp
@@ -11,6 +11,9 @@
 namespace box2d
 {
 
+namespace
+{
+
 /// Gets the position solver manifold in world coordinates for a circles-type manifold.
 /// @param xfA Transformation for body A.
 /// @param lp Local point. Location of shape A in local coordinates.
@@ -18,8 +21,8 @@ namespace box2d
 /// @param plp Point's local point. Location of shape B in local coordinates.
 /// @note The returned separation is the magnitude of the positional difference of the two points.
 ///   This is always a non-negative amount.
-static inline PositionSolverManifold GetForCircles(const Transformation& xfA, Vec2 lp,
-												   const Transformation& xfB, Vec2 plp)
+inline PositionSolverManifold GetForCircles(const Transformation& xfA, Vec2 lp,
+											const Transformation& xfB, Vec2 plp)
 {
 	const auto pointA = Transform(lp, xfA);
 	const auto pointB = Transform(plp, xfB);
@@ -39,8 +42,8 @@ static inline PositionSolverManifold GetForCircles(const Transformation& xfA, Ve
 /// @param plp Point's local point. Location for shape B in local coordinates.
 /// @return Separation is the dot-product of the positional difference between the two points in
 ///   the direction of the world normal.
-static inline PositionSolverManifold GetForFaceA(const Transformation& xfA, Vec2 lp, UnitVec2 ln,
-												 const Transformation& xfB, Vec2 plp)
+inline PositionSolverManifold GetForFaceA(const Transformation& xfA, Vec2 lp, UnitVec2 ln,
+										  const Transformation& xfB, Vec2 plp)
 {
 	const auto planePoint = Transform(lp, xfA);
 	const auto normal = Rotate(ln, xfA.q);
@@ -58,8 +61,8 @@ static inline PositionSolverManifold GetForFaceA(const Transformation& xfA, Vec2
 /// @param plp Point's local point. Location for shape A in local coordinates.
 /// @return Separation is the dot-product of the positional difference between the two points in
 ///   the direction of the world normal.
-static inline PositionSolverManifold GetForFaceB(const Transformation& xfB, Vec2 lp, UnitVec2 ln,
-												 const Transformation& xfA, Vec2 plp)
+inline PositionSolverManifold GetForFaceB(const Transformation& xfB, Vec2 lp, UnitVec2 ln,
+										  const Transformation& xfA, Vec2 plp)
 {
 	const auto planePoint = Transform(lp, xfB);
 	const auto normal = Rotate(ln, xfB.q);
@@ -69,26 +72,26 @@ static inline PositionSolverManifold GetForFaceB(const Transformation& xfB, Vec2
 	return PositionSolverManifold{-normal, clipPoint, separation};
 }
 
-PositionSolverManifold GetPSM(const Manifold& manifold, Manifold::size_type index,
+} // unnamed namespace
+
+PositionSolverManifold GetPSM(const Manifold& m, Manifold::size_type idx,
 							  const Transformation& xfA, const Transformation& xfB)
 {
-	assert(manifold.GetType() != Manifold::e_unset);
-	assert(manifold.GetPointCount() > 0);
+	assert(m.GetType() != Manifold::e_unset);
+	assert(m.GetPointCount() > 0);
 	
-	switch (manifold.GetType())
+	switch (m.GetType())
 	{
 	case Manifold::e_circles:
-		return GetForCircles(xfA, manifold.GetLocalPoint(),
-							 xfB, manifold.GetPoint(index).localPoint);
+		return GetForCircles(xfA, m.GetLocalPoint(), xfB, m.GetPoint(idx).localPoint);
 	case Manifold::e_faceA:
-		return GetForFaceA(xfA, manifold.GetLocalPoint(), manifold.GetLocalNormal(),
-						   xfB, manifold.GetPoint(index).localPoint);
+		return GetForFaceA(xfA, m.GetLocalPoint(), m.GetLocalNormal(), xfB, m.GetPoint(idx).localPoint);
 	case Manifold::e_faceB:
-		return GetForFaceB(xfB, manifold.GetLocalPoint(), manifold.GetLocalNormal(),
-						   xfA, manifold.GetPoint(index).localPoint);
+		return GetForFaceB(xfB, m.GetLocalPoint(), m.GetLocalNormal(), xfA, m.GetPoint(idx).localPoint);
 	case Manifold::e_unset:
 		break;
 	}
+
 	// should not be reached
 	return PositionSolverManifold{GetInvalid<UnitVec2>(), GetInvalid<Vec2>(), GetInvalid<float_t>()};
 }
