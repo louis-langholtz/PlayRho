e6a40544ed13c28ce07c687b652487ec1cd25988
louis-langholtz <lou_langholtz@me.com>
2017-02-18 09:57:50 -0700

    White space changes.

--- Box2D/Box2D/Dynamics/World.hpp
+++ Box2D/Box2D/Dynamics/World.hpp
@@ -1,21 +1,21 @@
 /*
-* Original work Copyright (c) 2006-2011 Erin Catto http://www.box2d.org
-* Modified work Copyright (c) 2016 Louis Langholtz https://github.com/louis-langholtz/Box2D
-*
-* This software is provided 'as-is', without any express or implied
-* warranty.  In no event will the authors be held liable for any damages
-* arising from the use of this software.
-* Permission is granted to anyone to use this software for any purpose,
-* including commercial applications, and to alter it and redistribute it
-* freely, subject to the following restrictions:
-* 1. The origin of this software must not be misrepresented; you must not
-* claim that you wrote the original software. If you use this software
-* in a product, an acknowledgment in the product documentation would be
-* appreciated but is not required.
-* 2. Altered source versions must be plainly marked as such, and must not be
-* misrepresented as being the original software.
-* 3. This notice may not be removed or altered from any source distribution.
-*/
+ * Original work Copyright (c) 2006-2011 Erin Catto http://www.box2d.org
+ * Modified work Copyright (c) 2016 Louis Langholtz https://github.com/louis-langholtz/Box2D
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
 
 #ifndef B2_WORLD_H
 #define B2_WORLD_H
@@ -418,7 +418,7 @@ private:
 	///      the appropriate contacts of the body.
 	///   3. Adds those contacts that are still enabled and still touching to the given island
 	///      (or resets the other bodies advancement).
- 	///   4. Adds to the island, those other bodies that haven't already been added of the contacts that got added.
+	///   4. Adds to the island, those other bodies that haven't already been added of the contacts that got added.
 	/// @note Precondition: there should be no lower TOI for which contacts have not already been processed.
 	/// @param[in,out] island Island. On return this may contain additional contacts or bodies.
 	/// @param[in,out] body A dynamic/accelerable body.
