f374f717b033d41b41996dc4c3a8529c51df0ff0
louis-langholtz <lou_langholtz@me.com>
2017-03-12 10:11:59 -0600

    Adds unit test case.

--- Box2D/UnitTests/StepConf.cpp
+++ Box2D/UnitTests/StepConf.cpp
@@ -33,6 +33,27 @@ TEST(StepConf, ByteSizeIs_96_168_or_320)
 	}
 }
 
+TEST(StepConf, CopyConstruction)
+{
+	const auto dt = RealNum{10};
+	const auto displacementMultiplier = RealNum{3.4f};
+
+	StepConf conf;
+	conf.set_dt(dt);
+	conf.displaceMultiplier = displacementMultiplier;
+
+	ASSERT_EQ(conf.get_inv_dt(), 1 / dt);
+	
+	EXPECT_EQ(StepConf{conf}.get_dt(), dt);
+	EXPECT_EQ(StepConf{conf}.get_inv_dt(), 1 / dt);
+	EXPECT_EQ(StepConf{conf}.displaceMultiplier, displacementMultiplier);
+	
+	const auto cdt = conf.get_dt() * 0.8f;
+	const auto newConf = StepConf{conf}.set_dt(cdt);
+	
+	EXPECT_EQ(newConf.get_dt(), cdt);
+}
+
 TEST(StepConf, maxTranslation)
 {
 	const auto v = RealNum(1);
