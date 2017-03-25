01014dd6493714d4728979c68f1f2e8a6aa7ba78
louis-langholtz <lou_langholtz@me.com>
2017-03-25 13:04:56 -0600

    Changes to be able to compile and run the HelloWorld application again.

--- Box2D/HelloWorld/HelloWorld.cpp
+++ Box2D/HelloWorld/HelloWorld.cpp
@@ -21,79 +21,72 @@
 
 #include <stdio.h>
 
+using namespace box2d;
+
 // This is a simple example of building and running a simulation
 // using Box2D. Here we create a large ground box and a small dynamic
 // box.
 // There are no graphics for this example. Box2D is meant to be used
 // with your rendering engine in your game engine.
-int main(int argc, char** argv)
+int main(int, char**)
 {
-	NOT_USED(argc);
-	NOT_USED(argv);
-
-	// Define the gravity vector.
-	Vec2 gravity(0.0f, -10.0f);
-
 	// Construct a world object, which will hold and simulate the rigid bodies.
-	World world(gravity);
+	World world;
 
 	// Define the ground body.
-	BodyDef groundBodyDef;
+	auto groundBodyDef = BodyDef{};
 	groundBodyDef.position = Vec2(0.0f, -10.0f);
 
 	// Call the body factory which allocates memory for the ground body
 	// from a pool and creates the ground box shape (also from a pool).
 	// The body is also added to the world.
-	Body* groundBody = world.Create(groundBodyDef);
+	const auto groundBody = world.CreateBody(groundBodyDef);
 
 	// Define the ground box shape.
 	// The extents are the half-widths of the box.
-	const auto groundBox = PolygonShape(50.0f, 10.0f);
+	auto groundBox = std::make_shared<PolygonShape>(50.0f, 10.0f);
 
 	// Add the ground fixture to the ground body.
-	groundBody->CreateFixture(FixtureDef{&groundBox, 0.0f});
+	groundBody->CreateFixture(groundBox);
 
 	// Define the dynamic body. We set its position and call the body factory.
-	BodyDef bodyDef;
+	auto bodyDef = BodyDef{};
 	bodyDef.type = BodyType::Dynamic;
 	bodyDef.position = Vec2(0.0f, 4.0f);
-	Body* body = world.Create(bodyDef);
+	const auto body = world.CreateBody(bodyDef);
 
 	// Define another box shape for our dynamic body.
-	const auto dynamicBox = PolygonShape(1.0f, 1.0f);
-
-	// Define the dynamic body fixture.
-	FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;
+	const auto dynamicBox = std::make_shared<PolygonShape>(1.0f, 1.0f);
 
 	// Set the box density to be non-zero, so it will be dynamic.
-	fixtureDef.density = 1.0f;
+	dynamicBox->SetDensity(1.0f);
 
 	// Override the default friction.
-	fixtureDef.friction = 0.3f;
+	dynamicBox->SetFriction(0.3f);
 
 	// Add the shape to the body.
-	body->CreateFixture(fixtureDef);
+	body->CreateFixture(dynamicBox);
 
 	// Prepare for simulation. Typically we use a time step of 1/60 of a
 	// second (60Hz) and 10 iterations. This provides a high quality simulation
 	// in most game scenarios.
-	RealNum timeStep = 1.0f / 60.0f;
-	int32 velocityIterations = 6;
-	int32 positionIterations = 2;
+	auto stepConf = StepConf{};
+	stepConf.set_dt(1.0f / 60.0f);
+	stepConf.regVelocityIterations = 6;
+	stepConf.regPositionIterations = 2;
 
 	// This is our little game loop.
-	for (int32 i = 0; i < 60; ++i)
+	for (auto i = 0; i < 60; ++i)
 	{
 		// Instruct the world to perform a single step of simulation.
 		// It is generally best to keep the time step and iterations fixed.
-		Step(world, timeStep, velocityIterations, positionIterations);
+		world.Step(stepConf);
 
 		// Now print the position and angle of the body.
-		Vec2 position = body->GetLocation();
-		RealNum angle = body->GetAngle();
+		const auto position = body->GetLocation();
+		const auto angle = body->GetAngle();
 
-		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle / 1_deg);
 	}
 
 	// When the world destructor is called, all bodies and joints are freed. This can
