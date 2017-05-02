6b85e8c51bd89e52a8235d5cf1c1879288bff07b
louis-langholtz <lou_langholtz@me.com>
2017-05-01 19:11:04 -0600

    Updates Testbed code for new use of units.

--- Box2D/Testbed/Framework/Test.cpp
+++ Box2D/Testbed/Framework/Test.cpp
@@ -772,7 +772,7 @@ void Test::Step(const Settings& settings, Drawer& drawer)
 
     stepConf.maxSubSteps           = static_cast<StepConf::iteration_type>(settings.maxSubSteps);
     
-    stepConf.maxTranslation = static_cast<decltype(stepConf.maxTranslation)>(settings.maxTranslation);
+    stepConf.maxTranslation = static_cast<decltype(stepConf.maxTranslation)>(settings.maxTranslation * Meter);
     stepConf.maxRotation = RealNum{settings.maxRotation} * Degree;
     
     stepConf.linearSlop = RealNum{settings.linearSlop} * Meter;
