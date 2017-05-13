6f20d0ea4220872989f27bf32244c2c4d63d03dd
louis-langholtz <lou_langholtz@me.com>
2017-05-13 14:09:45 -0600

    Adds ContactKey class and associated functions for use with key-based data structures.

---
+++ Box2D/Dynamics/Contacts/ContactKey.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/Box2D
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include <Box2D/Dynamics/Contacts/ContactKey.hpp>
+#include <Box2D/Dynamics/Contacts/Contact.hpp>
+#include <Box2D/Dynamics/Fixture.hpp>
+#include <Box2D/Dynamics/FixtureProxy.hpp>
+
+using namespace box2d;
+
+ContactKey ContactKey::Get(const Fixture* fixtureA, child_count_t childIndexA,
+                                const Fixture* fixtureB, child_count_t childIndexB) noexcept
+{
+#if 0
+    return (fixtureA < fixtureB)?
+  	  ContactKey{fixtureA, childIndexA, fixtureB, childIndexB}:
+  	  ContactKey{fixtureB, childIndexB, fixtureA, childIndexA};
+#else
+    const auto pidA = fixtureA->GetProxy(childIndexA)->proxyId;
+    const auto pidB = fixtureB->GetProxy(childIndexB)->proxyId;
+    return (pidA < pidB)? ContactKey{pidA, pidB}: ContactKey{pidB, pidA};
+#endif
+}
+
+ContactKey box2d::GetContactKey(const Contact& contact) noexcept
+{
+    return ContactKey::Get(contact.GetFixtureA(), contact.GetChildIndexA(),
+                           contact.GetFixtureB(), contact.GetChildIndexB());
+}
---
+++ Box2D/Dynamics/Contacts/ContactKey.hpp
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/Box2D
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#ifndef ContactKey_hpp
+#define ContactKey_hpp
+
+/// @file
+/// Declaration of the ContactKey class.
+
+#include <Box2D/Common/Settings.hpp>
+
+namespace box2d
+{
+    class Fixture;
+
+    class ContactKey
+    {
+    public:
+        static ContactKey Get(const Fixture* fixtureA, child_count_t childIndexA,
+                              const Fixture* fixtureB, child_count_t childIndexB) noexcept;
+        
+        static constexpr std::size_t Hash(const ContactKey& key) noexcept
+        {
+            return static_cast<std::size_t>(key.m_fp1) << 32 | static_cast<std::size_t>(key.m_fp2);
+        }
+
+        static constexpr int Compare(const ContactKey& lhs, const ContactKey& rhs)
+        {
+#if 0
+            if (lhs.m_fixtureA < rhs.m_fixtureA)
+            {
+                return -1;
+            }
+            if (lhs.m_fixtureA > rhs.m_fixtureA)
+            {
+                return +1;
+            }
+            if (lhs.m_fixtureB < rhs.m_fixtureB)
+            {
+                return -1;
+            }
+            if (lhs.m_fixtureB > rhs.m_fixtureB)
+            {
+                return +1;
+            }
+            if (lhs.m_childIndexA < rhs.m_childIndexA)
+            {
+                return -1;
+            }
+            if (lhs.m_childIndexA > rhs.m_childIndexA)
+            {
+                return +1;
+            }
+            if (lhs.m_childIndexB < rhs.m_childIndexB)
+            {
+                return -1;
+            }
+            if (lhs.m_childIndexB > rhs.m_childIndexB)
+            {
+                return +1;
+            }
+            // Entirely equal
+            return 0;
+#else
+            if (lhs.m_fp1 < rhs.m_fp1)
+            {
+                return -1;
+            }
+            if (lhs.m_fp1 > rhs.m_fp1)
+            {
+                return +1;
+            }
+            if (lhs.m_fp2 < rhs.m_fp2)
+            {
+                return -1;
+            }
+            if (lhs.m_fp2 > rhs.m_fp2)
+            {
+                return +1;
+            }
+            return 0;
+#endif
+        }
+        
+    private:
+#if 0
+        constexpr ContactKey(const Fixture* fixtureA, child_count_t childIndexA,
+                             const Fixture* fixtureB, child_count_t childIndexB) noexcept:
+        	m_fixtureA{fixtureA},
+        	m_fixtureB{fixtureB},
+        	m_childIndexA{childIndexA},
+        	m_childIndexB{childIndexB}
+        {
+            // Intentionally empty.
+        }
+
+        const Fixture* m_fixtureA;
+        const Fixture* m_fixtureB;
+        child_count_t m_childIndexA;
+        child_count_t m_childIndexB;
+#else
+        constexpr ContactKey(contact_count_t fp1, contact_count_t fp2) noexcept:
+        	m_fp1{fp1}, m_fp2{fp2}
+        {
+            // Intentionally empty.
+        }
+    
+        contact_count_t m_fp1;
+        contact_count_t m_fp2;
+#endif
+    };
+    
+    ContactKey GetContactKey(const Contact& contact) noexcept;
+
+} // namespace box2d
+
+namespace std
+{
+    template <>
+    struct less<box2d::ContactKey>
+    {
+        constexpr bool operator()(const box2d::ContactKey& lhs,
+                                  const box2d::ContactKey& rhs) const
+        {
+            return box2d::ContactKey::Compare(lhs, rhs) < 0;
+        }
+    };
+    
+    template <>
+    struct hash<box2d::ContactKey>
+    {
+        using argument_type = box2d::ContactKey;
+        using result_type = std::size_t;
+
+        constexpr std::size_t operator()(const box2d::ContactKey& key) const
+        {
+            return box2d::ContactKey::Hash(key);
+        }
+    };
+    
+    template <>
+    struct equal_to<box2d::ContactKey>
+    {
+        constexpr bool operator()( const box2d::ContactKey& lhs, const box2d::ContactKey& rhs ) const
+        {
+            return box2d::ContactKey::Compare(lhs, rhs) == 0;
+        }
+    };
+}
+
+#endif /* ContactKey_hpp */
