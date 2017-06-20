407af2ba21e80685362d9d9b6fa89dd5885140fb
louis-langholtz <lou_langholtz@me.com>
2017-06-19 23:37:32 -0600

    Adds unit test code for RayCastOutput.

---
+++ UnitTests/RayCastOutput.cpp
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/Box2D
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include "gtest/gtest.h"
+
+#include <Box2D/Collision/RayCastOutput.hpp>
+#include <Box2D/Collision/RayCastInput.hpp>
+
+#include <type_traits>
+
+using namespace box2d;
+
+TEST(RayCastOutput, ByteSize)
+{
+    EXPECT_EQ(sizeof(RayCastOutput), size_t(16));
+}
+
+TEST(RayCastOutput, Traits)
+{
+    EXPECT_TRUE(std::is_default_constructible<RayCastOutput>::value);
+    EXPECT_TRUE(std::is_nothrow_default_constructible<RayCastOutput>::value);
+    EXPECT_FALSE(std::is_trivially_default_constructible<RayCastOutput>::value);
+    
+    EXPECT_TRUE(std::is_constructible<RayCastOutput>::value);
+    EXPECT_TRUE(std::is_nothrow_constructible<RayCastOutput>::value);
+    EXPECT_FALSE(std::is_trivially_constructible<RayCastOutput>::value);
+    
+    EXPECT_TRUE(std::is_copy_constructible<RayCastOutput>::value);
+    EXPECT_TRUE(std::is_nothrow_copy_constructible<RayCastOutput>::value);
+    EXPECT_TRUE(std::is_trivially_copy_constructible<RayCastOutput>::value);
+    
+    EXPECT_TRUE(std::is_copy_assignable<RayCastOutput>::value);
+    EXPECT_TRUE(std::is_nothrow_copy_assignable<RayCastOutput>::value);
+    EXPECT_TRUE(std::is_trivially_copy_assignable<RayCastOutput>::value);
+    
+    EXPECT_TRUE(std::is_destructible<RayCastOutput>::value);
+    EXPECT_TRUE(std::is_nothrow_destructible<RayCastOutput>::value);
+    EXPECT_TRUE(std::is_trivially_destructible<RayCastOutput>::value);
+}
+
+TEST(RayCastOutput, DefaultConstruction)
+{
+    RayCastOutput foo;
+    EXPECT_FALSE(foo.hit);
+    EXPECT_FALSE(IsValid(foo.normal));
+    EXPECT_FALSE(IsValid(foo.fraction));
+}
+
+TEST(RayCastOutput, InitConstruction)
+{
+    const auto normal = UnitVec2::GetLeft();
+    const auto fraction = RealNum(0.8f);
+    RayCastOutput foo{normal, fraction};
+    EXPECT_TRUE(foo.hit);
+    EXPECT_EQ(foo.normal, normal);
+    EXPECT_EQ(foo.fraction, fraction);
+}
+
+TEST(RayCastOutput, RayCastFreeFunction)
+{
+    const auto radius = RealNum(0.1) * Meter;
+    const auto location = Vec2(5, 2) * Meter;
+    const auto p1 = Vec2(10, 2) * Meter;
+    const auto p2 = Vec2(0, 2) * Meter;
+    const auto maxFraction = RealNum(1);
+    auto input = RayCastInput{p1, p2, maxFraction};
+    const auto output = RayCast(radius, location, input);
+    EXPECT_TRUE(output.hit);
+    EXPECT_EQ(output.normal, UnitVec2::GetRight());
+    EXPECT_EQ(output.fraction, RealNum(0.49f));
+}
