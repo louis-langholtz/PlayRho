2530d5530f5d0519e4a674e7af2ed5070a5714ca
louis-langholtz <lou_langholtz@me.com>
2017-06-21 15:25:52 -0600

    Adds more unit test coverage of RopeJoint.

--- UnitTests/RopeJoint.cpp
+++ UnitTests/RopeJoint.cpp
@@ -23,6 +23,8 @@
 #include <Box2D/Dynamics/Joints/RopeJoint.hpp>
 #include <Box2D/Dynamics/Body.hpp>
 #include <Box2D/Dynamics/BodyDef.hpp>
+#include <Box2D/Dynamics/World.hpp>
+#include <Box2D/Collision/Shapes/DiskShape.hpp>
 
 using namespace box2d;
 
@@ -111,3 +113,25 @@ TEST(RopeJoint, GetRopeJointDef)
     EXPECT_EQ(cdef.localAnchorB, localAnchorB);
     EXPECT_EQ(cdef.maxLength, Length{0});
 }
+
+TEST(RopeJoint, WithDynamicCircles)
+{
+    const auto circle = std::make_shared<DiskShape>(RealNum{0.2f} * Meter);
+    auto world = World{WorldDef{}.UseGravity(Vec2_zero * MeterPerSquareSecond)};
+    const auto p1 = Vec2{-1, 0} * Meter;
+    const auto p2 = Vec2{+1, 0} * Meter;
+    const auto b1 = world.CreateBody(BodyDef{}.UseType(BodyType::Dynamic).UseLocation(p1));
+    const auto b2 = world.CreateBody(BodyDef{}.UseType(BodyType::Dynamic).UseLocation(p2));
+    b1->CreateFixture(circle);
+    b2->CreateFixture(circle);
+    const auto jd = RopeJointDef{b1, b2};
+    world.CreateJoint(jd);
+    Step(world, Time{Second * RealNum{1}});
+    EXPECT_GT(b1->GetLocation().x, RealNum(-1) * Meter);
+    EXPECT_EQ(b1->GetLocation().y, RealNum(0) * Meter);
+    EXPECT_LT(b2->GetLocation().x, RealNum(+1) * Meter);
+    EXPECT_EQ(b2->GetLocation().y, RealNum(0) * Meter);
+    EXPECT_EQ(b1->GetAngle(), RealNum{0} * Degree);
+    EXPECT_EQ(b2->GetAngle(), RealNum{0} * Degree);
+}
+
