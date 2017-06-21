429b925e5dd35b23c3e1ba5f1133815ebbcd1edd
louis-langholtz <lou_langholtz@me.com>
2017-06-21 15:30:43 -0600

    Adds more unit test code coverage for GearJoint.

--- UnitTests/GearJoint.cpp
+++ UnitTests/GearJoint.cpp
@@ -24,6 +24,8 @@
 #include <Box2D/Dynamics/Joints/RevoluteJoint.hpp>
 #include <Box2D/Dynamics/Body.hpp>
 #include <Box2D/Dynamics/BodyDef.hpp>
+#include <Box2D/Dynamics/World.hpp>
+#include <Box2D/Collision/Shapes/DiskShape.hpp>
 
 using namespace box2d;
 
@@ -121,4 +123,31 @@ TEST(GearJoint, GetGearJointDef)
     EXPECT_EQ(cdef.joint1, &revJoint1);
     EXPECT_EQ(cdef.joint2, &revJoint2);
     EXPECT_EQ(cdef.ratio, RealNum(1));
+}
+
+
+TEST(GearJoint, WithDynamicCircles)
+{
+    const auto circle = std::make_shared<DiskShape>(RealNum{0.2f} * Meter);
+    auto world = World{WorldDef{}.UseGravity(Vec2_zero * MeterPerSquareSecond)};
+    const auto p1 = Vec2{-1, 0} * Meter;
+    const auto p2 = Vec2{+1, 0} * Meter;
+    const auto b1 = world.CreateBody(BodyDef{}.UseType(BodyType::Dynamic).UseLocation(p1));
+    const auto b2 = world.CreateBody(BodyDef{}.UseType(BodyType::Dynamic).UseLocation(p2));
+    b1->CreateFixture(circle);
+    b2->CreateFixture(circle);
+    RevoluteJointDef rdef{b1, b2, Vec2_zero * Meter};
+    RevoluteJoint revJoint1{rdef};
+    RevoluteJoint revJoint2{rdef};
+    GearJointDef def;
+    def.joint1 = &revJoint1;
+    def.joint2 = &revJoint2;
+    world.CreateJoint(def);
+    Step(world, Time{Second * RealNum{1}});
+    EXPECT_NEAR(double(RealNum{b1->GetLocation().x / Meter}), -1.0, 0.001);
+    EXPECT_NEAR(double(RealNum{b1->GetLocation().y / Meter}), 0.0, 0.001);
+    EXPECT_NEAR(double(RealNum{b2->GetLocation().x / Meter}), +1.0, 0.01);
+    EXPECT_NEAR(double(RealNum{b2->GetLocation().y / Meter}), 0.0, 0.01);
+    EXPECT_EQ(b1->GetAngle(), RealNum{0} * Degree);
+    EXPECT_EQ(b2->GetAngle(), RealNum{0} * Degree);
 }
