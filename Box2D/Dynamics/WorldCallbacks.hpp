88d772255392b2cff2ecfb03161bd39b5633ba6c
louis-langholtz <lou_langholtz@me.com>
2017-06-22 12:05:46 -0600

    Converts ContactListener class into a pure virtual interface class.

--- Box2D/Dynamics/WorldCallbacks.hpp
+++ Box2D/Dynamics/WorldCallbacks.hpp
@@ -89,16 +89,16 @@ private:
     count_t count = 0;
 };
 
-/// An interface for "listeners" for contacts.
+/// @brief A pure-virtual interface for "listeners" for contacts.
 ///
-/// Implement this class to get contact information. You can use these results for
-/// things like sounds and game logic. You can also get contact results by
-/// traversing the contact lists after the time step. However, you might miss
-/// some contacts because continuous physics leads to sub-stepping.
-/// Additionally you may receive multiple callbacks for the same contact in a
-/// single time step.
-/// You should strive to make your callbacks efficient because there may be
-/// many callbacks per time step.
+/// @details Implement this class to get contact information. You can use these results
+///   for things like sounds and game logic. You can also get contact results by
+///   traversing the contact lists after the time step. However, you might miss
+///   some contacts because continuous physics leads to sub-stepping.
+///   Additionally you may receive multiple callbacks for the same contact in a
+///   single time step.
+///   You should strive to make your callbacks efficient because there may be
+///   many callbacks per time step.
 ///
 /// @warning You cannot create/destroy Box2D entities inside these callbacks.
 ///
@@ -107,56 +107,56 @@ class ContactListener
 public:
     using iteration_type = unsigned;
 
-    virtual ~ContactListener() {}
+    virtual ~ContactListener() = default;
 
-    /// Called when two fixtures begin to touch.
-    virtual void BeginContact(Contact& contact) { NOT_USED(contact); }
+    /// @brief Called when two fixtures begin to touch.
+    virtual void BeginContact(Contact& contact) = 0;
 
-    /// End contact callback.
-    /// @details
-    /// Called when the contact's "touching" property becomes false, or just before the contact
-    /// is destroyed.
+    /// @brief End contact callback.
+    ///
+    /// @details Called when the contact's "touching" property becomes false, or just before
+    ///   the contactis destroyed.
+    ///
     /// @note This contact persists until the broadphase determines there's no overlap anymore
     ///   between the two fixtures.
     /// @note If the contact's "touching" property becomes true again, BeginContact will be called
     ///   again for this contact.
-    /// @sa Contact::IsTouching().
+    ///
     /// @param contact Contact that's about to be destroyed or whose "touching" property has become
     ///   false.
-    virtual void EndContact(Contact& contact) { NOT_USED(contact); }
-
-    /// Pre-solve callback.
-    /// @details
-    /// This is called after a contact is updated. This allows you to inspect a
-    /// contact before it goes to the solver. If you are careful, you can modify the
-    /// contact manifold (e.g. disable contact).
-    /// A copy of the old manifold is provided so that you can detect changes.
+    ///
+    /// @sa Contact::IsTouching().
+    ///
+    virtual void EndContact(Contact& contact) = 0;
+    
+    /// @brief Pre-solve callback.
+    ///
+    /// @details This is called after a contact is updated. This allows you to inspect
+    ///   a contact before it goes to the solver. If you are careful, you can modify the
+    ///   contact manifold (e.g. disable contact). A copy of the old manifold is provided
+    ///   so that you can detect changes.
+    ///
     /// @note This is called only for awake bodies.
     /// @note This is called even when the number of contact points is zero.
     /// @note This is not called for sensors.
     /// @note If you set the number of contact points to zero, you will not
-    /// get an EndContact callback. However, you may get a BeginContact callback
-    /// the next step.
-    virtual void PreSolve(Contact& contact, const Manifold& oldManifold)
-    {
-        NOT_USED(contact);
-        NOT_USED(oldManifold);
-    }
+    ///   get an EndContact callback. However, you may get a BeginContact callback
+    ///   the next step.
+    ///
+    virtual void PreSolve(Contact& contact, const Manifold& oldManifold) = 0;
 
-    /// Post-solve callback.
-    /// @details
-    /// This lets you inspect a contact after the solver is finished. This is useful
-    /// for inspecting impulses.
+    /// @brief Post-solve callback.
+    ///
+    /// @details This lets you inspect a contact after the solver is finished. This is useful
+    ///   for inspecting impulses.
+    ///
     /// @note The contact manifold does not include time of impact impulses, which can be
-    /// arbitrarily large if the sub-step is small. Hence the impulse is provided explicitly
-    /// in a separate data structure.
+    ///   arbitrarily large if the sub-step is small. Hence the impulse is provided explicitly
+    ///   in a separate data structure.
     /// @note This is only called for contacts that are touching, solid, and awake.
-    virtual void PostSolve(Contact& contact, const ContactImpulsesList& impulse, iteration_type solved)
-    {
-        NOT_USED(contact);
-        NOT_USED(impulse);
-        NOT_USED(solved);
-    }
+    ///
+    virtual void PostSolve(Contact& contact, const ContactImpulsesList& impulses,
+                           iteration_type solved) = 0;
 };
 
 } // namespace box2d
