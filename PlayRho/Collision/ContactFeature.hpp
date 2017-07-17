6a596c5b4c0f8af7832fe33af66d1444ecc11395
louis-langholtz <lou_langholtz@me.com>
2017-07-17 09:27:38 -0600

    Reformats code and updates documentation comments.

--- PlayRho/Collision/ContactFeature.hpp
+++ PlayRho/Collision/ContactFeature.hpp
@@ -3,17 +3,19 @@
  * Modified work Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
  *
  * This software is provided 'as-is', without any express or implied
- * warranty.  In no event will the authors be held liable for any damages
+ * warranty. In no event will the authors be held liable for any damages
  * arising from the use of this software.
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
+ *
  * 1. The origin of this software must not be misrepresented; you must not
- * claim that you wrote the original software. If you use this software
- * in a product, an acknowledgment in the product documentation would be
- * appreciated but is not required.
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
  * 2. Altered source versions must be plainly marked as such, and must not be
- * misrepresented as being the original software.
+ *    misrepresented as being the original software.
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
@@ -24,64 +26,76 @@
 
 namespace playrho
 {
-    /// Contact Feature.
-    /// @details The features that intersect to form the contact point.
-    /// @note This structure is designed to be compact and passed-by-value.
-    /// @note This data structure is 4-bytes large.
-    struct ContactFeature
+
+/// @brief Contact Feature.
+/// @details The features that intersect to form the contact point.
+/// @note This structure is designed to be compact and passed-by-value.
+/// @note This data structure is 4-bytes large.
+/// @note Possible type combinations are:
+///   vertex-vertex,
+///   vertex-face,
+///   face-vertex, or
+///   face-face.
+struct ContactFeature
+{
+    using Index = std::uint8_t; ///< Index type.
+
+    enum Type : std::uint8_t
     {
-        using Index = std::uint8_t; ///< Index type.
-        
-        enum Type: std::uint8_t
-        {
-            e_vertex = 0,
-            e_face = 1
-        };
-                
-        // Fit data into 4-byte large structure...
-        
-        Type typeA; ///< The feature type on shape A
-        Index indexA; ///< Feature index on shape A
-        Type typeB; ///< The feature type on shape B
-        Index indexB; ///< Feature index on shape B
+        e_vertex = 0,
+        e_face = 1
     };
-    
-    constexpr ContactFeature GetVertexVertexContactFeature(ContactFeature::Index a, ContactFeature::Index b) noexcept
-    {
-        return ContactFeature{ContactFeature::e_vertex, a, ContactFeature::e_vertex, b};
-    }
 
-    constexpr ContactFeature GetVertexFaceContactFeature(ContactFeature::Index a, ContactFeature::Index b) noexcept
-    {
-        return ContactFeature{ContactFeature::e_vertex, a, ContactFeature::e_face, b};
-    }
-    
-    constexpr ContactFeature GetFaceVertexContactFeature(ContactFeature::Index a, ContactFeature::Index b) noexcept
-    {
-        return ContactFeature{ContactFeature::e_face, a, ContactFeature::e_vertex, b};
-    }
-    
-    constexpr ContactFeature GetFaceFaceContactFeature(ContactFeature::Index a, ContactFeature::Index b) noexcept
-    {
-        return ContactFeature{ContactFeature::e_face, a, ContactFeature::e_face, b};
-    }
-        
-    /// Flips contact features information.
-    constexpr ContactFeature Flip(ContactFeature val) noexcept
-    {
-        return ContactFeature{val.typeB, val.indexB, val.typeA, val.indexA};
-    }
-    
-    constexpr bool operator==(ContactFeature lhs, ContactFeature rhs) noexcept
-    {
-        return (lhs.typeA == rhs.typeA) && (lhs.typeB == rhs.typeB) && (lhs.indexA == rhs.indexA) && (lhs.indexB == rhs.indexB);
-    }
+    // Fit data into 4-byte large structure...
 
-    constexpr bool operator!=(ContactFeature lhs, ContactFeature rhs) noexcept
-    {
-        return (lhs.typeA != rhs.typeA) || (lhs.typeB != rhs.typeB) || (lhs.indexA != rhs.indexA) || (lhs.indexB != rhs.indexB);
-    }
-    
+    Type typeA; ///< The feature type on shape A
+    Index indexA; ///< Feature index on shape A
+    Type typeB; ///< The feature type on shape B
+    Index indexB; ///< Feature index on shape B
+};
+
+constexpr ContactFeature GetVertexVertexContactFeature(ContactFeature::Index a,
+                                                       ContactFeature::Index b) noexcept
+{
+    return ContactFeature{ContactFeature::e_vertex, a, ContactFeature::e_vertex, b};
+}
+
+constexpr ContactFeature GetVertexFaceContactFeature(ContactFeature::Index a,
+                                                     ContactFeature::Index b) noexcept
+{
+    return ContactFeature{ContactFeature::e_vertex, a, ContactFeature::e_face, b};
+}
+
+constexpr ContactFeature GetFaceVertexContactFeature(ContactFeature::Index a,
+                                                     ContactFeature::Index b) noexcept
+{
+    return ContactFeature{ContactFeature::e_face, a, ContactFeature::e_vertex, b};
+}
+
+constexpr ContactFeature GetFaceFaceContactFeature(ContactFeature::Index a,
+                                                   ContactFeature::Index b) noexcept
+{
+    return ContactFeature{ContactFeature::e_face, a, ContactFeature::e_face, b};
+}
+
+/// Flips contact features information.
+constexpr ContactFeature Flip(ContactFeature val) noexcept
+{
+    return ContactFeature{val.typeB, val.indexB, val.typeA, val.indexA};
+}
+
+constexpr bool operator==(ContactFeature lhs, ContactFeature rhs) noexcept
+{
+    return (lhs.typeA == rhs.typeA) && (lhs.typeB == rhs.typeB) && (lhs.indexA == rhs.indexA) &&
+           (lhs.indexB == rhs.indexB);
+}
+
+constexpr bool operator!=(ContactFeature lhs, ContactFeature rhs) noexcept
+{
+    return (lhs.typeA != rhs.typeA) || (lhs.typeB != rhs.typeB) || (lhs.indexA != rhs.indexA) ||
+           (lhs.indexB != rhs.indexB);
+}
+
 }; // namespace playrho
 
 #endif /* ContactFeature_hpp */
