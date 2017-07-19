5d1cdf47274449afa015e7a102541d9f6cb4f2f1
louis-langholtz <lou_langholtz@me.com>
2017-07-19 13:16:18 -0600

    Updates and adds unit test coverage of DiskShape.

--- UnitTests/DiskShape.cpp
+++ UnitTests/DiskShape.cpp
@@ -2,17 +2,19 @@
  * Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
  *
  * This software is provided 'as-is', without any express or implied
- * warranty.  In no event will the authors be held liable for any damages
+ * warranty. In no event will the authors be held liable for any damages
  * arising from the use of this software.
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
+ *
  * 1. The origin of this software must not be misrepresented; you must not
- * claim that you wrote the original software. If you use this software
- * in a product, an acknowledgment in the product documentation would be
- * appreciated but is not required.
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
  * 2. Altered source versions must be plainly marked as such, and must not be
- * misrepresented as being the original software.
+ *    misrepresented as being the original software.
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
@@ -24,21 +26,12 @@ using namespace playrho;
 
 TEST(DiskShape, ByteSize)
 {
-    if (sizeof(Real) == 4)
+    switch (sizeof(Real))
     {
-        EXPECT_EQ(sizeof(DiskShape), std::size_t(32));
-    }
-    else if (sizeof(Real) == 8)
-    {
-        EXPECT_EQ(sizeof(DiskShape), std::size_t(56));
-    }
-    else if (sizeof(Real) == 16)
-    {
-        EXPECT_EQ(sizeof(DiskShape), std::size_t(112));
-    }
-    else
-    {
-        FAIL();
+        case  4: EXPECT_EQ(sizeof(DiskShape), std::size_t(32)); break;
+        case  8: EXPECT_EQ(sizeof(DiskShape), std::size_t(56)); break;
+        case 16: EXPECT_EQ(sizeof(DiskShape), std::size_t(112)); break;
+        default: FAIL(); break;
     }
 }
 
@@ -67,6 +60,34 @@ TEST(DiskShape, InitConstruction)
     EXPECT_EQ(foo.GetRadius(), radius);
     EXPECT_EQ(foo.GetLocation().x, position.x);
     EXPECT_EQ(foo.GetLocation().y, position.y);
+}
+
+TEST(DiskShape, GetInvalidChildThrows)
+{
+    DiskShape foo{};
+    
+    ASSERT_EQ(foo.GetChildCount(), ChildCounter{1});
+    EXPECT_NO_THROW(foo.GetChild(0));
+    EXPECT_THROW(foo.GetChild(1), InvalidArgument);
+}
+
+class Visitor: public Shape::Visitor
+{
+public:
+    void Visit(const DiskShape&) override
+    {
+        visited = true;
+    }
+    bool visited = false;
+};
+
+TEST(DiskShape, Accept)
+{
+    DiskShape foo{};
+    Visitor v;
+    ASSERT_FALSE(v.visited);
+    foo.Accept(v);
+    EXPECT_TRUE(v.visited);
 }
 
 TEST(DiskShape, TestPoint)
