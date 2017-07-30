a984eb91c71ab8dfedfc4706cd529cae0eb7de0b
louis-langholtz <lou_langholtz@me.com>
2017-07-30 17:39:47 -0600

    Splits out GearJointDef code out from GearJoint and into its own .hpp and .cpp files.

--- PlayRho/Dynamics/Joints/GearJoint.cpp
+++ PlayRho/Dynamics/Joints/GearJoint.cpp
@@ -1,21 +1,23 @@
 /*
-* Original work Copyright (c) 2007-2011 Erin Catto http://www.box2d.org
-* Modified work Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
-*
-* This software is provided 'as-is', without any express or implied
-* warranty.  In no event will the authors be held liable for any damages
-* arising from the use of this software.
-* Permission is granted to anyone to use this software for any purpose,
-* including commercial applications, and to alter it and redistribute it
-* freely, subject to the following restrictions:
-* 1. The origin of this software must not be misrepresented; you must not
-* claim that you wrote the original software. If you use this software
-* in a product, an acknowledgment in the product documentation would be
-* appreciated but is not required.
-* 2. Altered source versions must be plainly marked as such, and must not be
-* misrepresented as being the original software.
-* 3. This notice may not be removed or altered from any source distribution.
-*/
+ * Original work Copyright (c) 2007-2011 Erin Catto http://www.box2d.org
+ * Modified work Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
 
 #include <PlayRho/Dynamics/Joints/GearJoint.hpp>
 #include <PlayRho/Dynamics/Joints/RevoluteJoint.hpp>
@@ -411,14 +413,4 @@ void GearJoint::SetRatio(Real ratio)
 {
     assert(IsValid(ratio));
     m_ratio = ratio;
-}
-
-GearJointDef playrho::GetGearJointDef(const GearJoint& joint) noexcept
-{
-    auto def = GearJointDef{joint.GetJoint1(), joint.GetJoint2()};
-    
-    Set(def, joint);
-    def.ratio = joint.GetRatio();
-
-    return def;
 }
--- PlayRho/Dynamics/Joints/GearJoint.hpp
+++ PlayRho/Dynamics/Joints/GearJoint.hpp
@@ -1,60 +1,32 @@
 /*
-* Original work Copyright (c) 2006-2011 Erin Catto http://www.box2d.org
-* Modified work Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
-*
-* This software is provided 'as-is', without any express or implied
-* warranty.  In no event will the authors be held liable for any damages
-* arising from the use of this software.
-* Permission is granted to anyone to use this software for any purpose,
-* including commercial applications, and to alter it and redistribute it
-* freely, subject to the following restrictions:
-* 1. The origin of this software must not be misrepresented; you must not
-* claim that you wrote the original software. If you use this software
-* in a product, an acknowledgment in the product documentation would be
-* appreciated but is not required.
-* 2. Altered source versions must be plainly marked as such, and must not be
-* misrepresented as being the original software.
-* 3. This notice may not be removed or altered from any source distribution.
-*/
+ * Original work Copyright (c) 2006-2011 Erin Catto http://www.box2d.org
+ * Modified work Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
 
 #ifndef B2_GEAR_JOINT_H
 #define B2_GEAR_JOINT_H
 
 #include <PlayRho/Dynamics/Joints/Joint.hpp>
+#include <PlayRho/Dynamics/Joints/GearJointDef.hpp>
 #include <PlayRho/Common/BoundedValue.hpp>
 
 namespace playrho {
-
-/// @brief Gear joint definition.
-/// @details This definition requires two existing
-/// revolute or prismatic joints (any combination will work).
-struct GearJointDef : public JointBuilder<GearJointDef>
-{
-    using super = JointBuilder<GearJointDef>;
-
-    GearJointDef(NonNull<Joint*> j1, NonNull<Joint*> j2) noexcept:
-        super{super{JointType::Gear}.UseBodyA(j1->GetBodyB()).UseBodyB(j2->GetBodyB())},
-        joint1{j1}, joint2{j2}
-    {
-        // Intentionally empty.
-    }
-    
-    GearJointDef& UseRatio(Real v) noexcept
-    {
-        ratio = v;
-        return *this;
-    }
-
-    /// The first revolute/prismatic joint attached to the gear joint.
-    NonNull<Joint*> joint1;
-
-    /// The second revolute/prismatic joint attached to the gear joint.
-    NonNull<Joint*> joint2;
-
-    /// The gear ratio.
-    /// @see GearJoint for explanation.
-    Real ratio = Real{1};
-};
 
 /// @brief Gear joint.
 /// @details A gear joint is used to connect two joints together. Either joint
@@ -141,8 +113,6 @@ inline Real GearJoint::GetRatio() const noexcept
 {
     return m_ratio;
 }
-
-GearJointDef GetGearJointDef(const GearJoint& joint) noexcept;
     
 } // namespace playrho
 
