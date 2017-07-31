9eca0f91ecb9f5d062dedc184b485e4746b981ae
louis-langholtz <lou_langholtz@me.com>
2017-07-30 20:57:23 -0600

    Splits out RopeJointDef code out from RopeJoint and into its own .hpp and .cpp files.

--- PlayRho/Dynamics/Joints/RopeJoint.hpp
+++ PlayRho/Dynamics/Joints/RopeJoint.hpp
@@ -1,51 +1,31 @@
 /*
-* Original work Copyright (c) 2006-2011 Erin Catto http://www.box2d.org
-* Modified work Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
-*
-* This software is provided 'as-is', without any express or implied
-* warranty.  In no event will the authors be held liable for any damages
-* arising from the use of this software.
-* Permission is granted to anyone to use this software for any purpose,
-* including commercial applications, and to alter it and redistribute it
-* freely, subject to the following restrictions:
-* 1. The origin of this software must not be misrepresented; you must not
-* claim that you wrote the original software. If you use this software
-* in a product, an acknowledgment in the product documentation would be
-* appreciated but is not required.
-* 2. Altered source versions must be plainly marked as such, and must not be
-* misrepresented as being the original software.
-* 3. This notice may not be removed or altered from any source distribution.
-*/
+ * Original work Copyright (c) 2006-2011 Erin Catto http://www.box2d.org
+ * Modified work Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
 
 #ifndef B2_ROPE_JOINT_H
 #define B2_ROPE_JOINT_H
 
 #include <PlayRho/Dynamics/Joints/Joint.hpp>
+#include <PlayRho/Dynamics/Joints/RopeJointDef.hpp>
 
 namespace playrho {
-
-/// @brief Rope joint definition.
-/// @details This requires two body anchor points and a maximum lengths.
-/// @note By default the connected objects will not collide.
-/// @see collideConnected in JointDef.
-struct RopeJointDef : public JointBuilder<RopeJointDef>
-{
-    using super = JointBuilder<RopeJointDef>;
-
-    constexpr RopeJointDef() noexcept: super{JointType::Rope} {}
-
-    constexpr RopeJointDef(Body* bodyA, Body* bodyB) noexcept:
-        super{super{JointType::Rope}.UseBodyA(bodyA).UseBodyB(bodyB)} {}
-
-    /// The local anchor point relative to bodyA's origin.
-    Length2D localAnchorA = Length2D{Real(-1) * Meter, Real(0) * Meter};
-
-    /// The local anchor point relative to bodyB's origin.
-    Length2D localAnchorB = Length2D{Real(1) * Meter, Real(0) * Meter};
-
-    /// The maximum length of the rope.
-    Length maxLength = Length{0};
-};
 
 /// @brief Rope joint.
 /// @details A rope joint enforces a maximum distance between two points
@@ -81,9 +61,11 @@ public:
 
 private:
 
-    void InitVelocityConstraints(BodyConstraintsMap& bodies, const StepConf& step, const ConstraintSolverConf& conf) override;
+    void InitVelocityConstraints(BodyConstraintsMap& bodies, const StepConf& step,
+                                 const ConstraintSolverConf& conf) override;
     bool SolveVelocityConstraints(BodyConstraintsMap& bodies, const StepConf& step) override;
-    bool SolvePositionConstraints(BodyConstraintsMap& bodies, const ConstraintSolverConf& conf) const override;
+    bool SolvePositionConstraints(BodyConstraintsMap& bodies,
+                                  const ConstraintSolverConf& conf) const override;
 
     // Solver shared
     Length2D m_localAnchorA;
@@ -99,8 +81,6 @@ private:
     Mass m_mass = Mass{0};
     LimitState m_state = e_inactiveLimit;
 };
-
-RopeJointDef GetRopeJointDef(const RopeJoint& joint) noexcept;
 
 } // namespace playrho
 
---
+++ PlayRho/Dynamics/Joints/RopeJointDef.hpp
@@ -0,0 +1,69 @@
+/*
+ * Original work Copyright (c) 2006-2011 Erin Catto http://www.box2d.org
+ * Modified work Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#ifndef RopeJointDef_hpp
+#define RopeJointDef_hpp
+
+#include <PlayRho/Dynamics/Joints/JointDef.hpp>
+#include <PlayRho/Common/BoundedValue.hpp>
+#include <PlayRho/Common/Math.hpp>
+
+namespace playrho {
+
+class RopeJoint;
+
+/// @brief Rope joint definition.
+/// @details This requires two body anchor points and a maximum lengths.
+/// @note By default the connected objects will not collide.
+/// @see collideConnected in JointDef.
+struct RopeJointDef : public JointBuilder<RopeJointDef>
+{
+    using super = JointBuilder<RopeJointDef>;
+    
+    constexpr RopeJointDef() noexcept: super{JointType::Rope} {}
+    
+    constexpr RopeJointDef(Body* bodyA, Body* bodyB) noexcept:
+    super{super{JointType::Rope}.UseBodyA(bodyA).UseBodyB(bodyB)} {}
+    
+    constexpr RopeJointDef& UseMaxLength(Length v) noexcept;
+    
+    /// The local anchor point relative to bodyA's origin.
+    Length2D localAnchorA = Length2D{Real(-1) * Meter, Real(0) * Meter};
+    
+    /// The local anchor point relative to bodyB's origin.
+    Length2D localAnchorB = Length2D{Real(1) * Meter, Real(0) * Meter};
+    
+    /// The maximum length of the rope.
+    Length maxLength = Length{0};
+};
+
+constexpr RopeJointDef& RopeJointDef::UseMaxLength(Length v) noexcept
+{
+    maxLength = v;
+    return *this;
+}
+
+RopeJointDef GetRopeJointDef(const RopeJoint& joint) noexcept;
+
+} // namespace playrho
+
+
+#endif /* RopeJointDef_hpp */
