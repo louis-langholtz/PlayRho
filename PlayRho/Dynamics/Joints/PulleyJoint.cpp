25b47a820a7fcbedf4ec41df3f86880fc273f188
louis-langholtz <lou_langholtz@me.com>
2017-07-30 20:38:41 -0600

    Splits out PulleyJointDef code out from PulleyJoint and into its own .hpp and .cpp files.

--- PlayRho/Dynamics/Joints/PulleyJoint.cpp
+++ PlayRho/Dynamics/Joints/PulleyJoint.cpp
@@ -3,17 +3,19 @@
  * Modified work Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
  *
  * This software is provided 'as-is', without any express or implied
- * warranty.  In no event will the authors be held liable for any damages
+ * warranty. In no event will the authors be held liable for any damages
  * arising from the use of this software.
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
+ *
  * 1. The origin of this software must not be misrepresented; you must not
- * claim that you wrote the original software. If you use this software
- * in a product, an acknowledgment in the product documentation would be
- * appreciated but is not required.
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
  * 2. Altered source versions must be plainly marked as such, and must not be
- * misrepresented as being the original software.
+ *    misrepresented as being the original software.
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
@@ -36,20 +38,6 @@ using namespace playrho;
 // J = -[u1 cross(r1, u1) ratio * u2  ratio * cross(r2, u2)]
 // K = J * invM * JT
 //   = invMass1 + invI1 * cross(r1, u1)^2 + ratio^2 * (invMass2 + invI2 * cross(r2, u2)^2)
-
-PulleyJointDef::PulleyJointDef(NonNull<Body*> bA, NonNull<Body*> bB,
-                               const Length2D groundA, const Length2D groundB,
-                               const Length2D anchorA, const Length2D anchorB):
-    super{super{JointType::Pulley}.UseBodyA(bA).UseBodyB(bB).UseCollideConnected(true)},
-    groundAnchorA{groundA},
-    groundAnchorB{groundB},
-    localAnchorA{GetLocalPoint(*bA, anchorA)},
-    localAnchorB{GetLocalPoint(*bB, anchorB)},
-    lengthA{GetLength(anchorA - groundA)},
-    lengthB{GetLength(anchorB - groundB)}
-{
-    // Intentionally empty.
-}
 
 PulleyJoint::PulleyJoint(const PulleyJointDef& def):
     Joint(def),
@@ -250,21 +238,4 @@ void PulleyJoint::ShiftOrigin(const Length2D newOrigin)
 {
     m_groundAnchorA -= newOrigin;
     m_groundAnchorB -= newOrigin;
-}
-
-PulleyJointDef playrho::GetPulleyJointDef(const PulleyJoint& joint) noexcept
-{
-    auto def = PulleyJointDef{};
-    
-    Set(def, joint);
-    
-    def.groundAnchorA = joint.GetGroundAnchorA();
-    def.groundAnchorB = joint.GetGroundAnchorB();
-    def.localAnchorA = joint.GetLocalAnchorA();
-    def.localAnchorB = joint.GetGroundAnchorB();
-    def.lengthA = joint.GetLengthA();
-    def.lengthB = joint.GetLengthB();
-    def.ratio = joint.GetRatio();
-    
-    return def;
 }
--- PlayRho/Dynamics/Joints/PulleyJoint.hpp
+++ PlayRho/Dynamics/Joints/PulleyJoint.hpp
@@ -3,17 +3,19 @@
  * Modified work Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
  *
  * This software is provided 'as-is', without any express or implied
- * warranty.  In no event will the authors be held liable for any damages
+ * warranty. In no event will the authors be held liable for any damages
  * arising from the use of this software.
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
+ *
  * 1. The origin of this software must not be misrepresented; you must not
- * claim that you wrote the original software. If you use this software
- * in a product, an acknowledgment in the product documentation would be
- * appreciated but is not required.
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
  * 2. Altered source versions must be plainly marked as such, and must not be
- * misrepresented as being the original software.
+ *    misrepresented as being the original software.
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
@@ -21,53 +23,9 @@
 #define B2_PULLEY_JOINT_H
 
 #include <PlayRho/Dynamics/Joints/Joint.hpp>
-#include <PlayRho/Common/BoundedValue.hpp>
+#include <PlayRho/Dynamics/Joints/PulleyJointDef.hpp>
 
 namespace playrho {
-
-/// @brief Pulley joint definition.
-/// @details This requires two ground anchors, two dynamic body anchor points, and a pulley ratio.
-struct PulleyJointDef : public JointBuilder<PulleyJointDef>
-{
-    using super = JointBuilder<PulleyJointDef>;
-
-    PulleyJointDef() noexcept: super{JointType::Pulley}
-    {
-        collideConnected = true;
-    }
-
-    /// Initialize the bodies, anchors, lengths, max lengths, and ratio using the world anchors.
-    PulleyJointDef(NonNull<Body*> bodyA, NonNull<Body*> bodyB,
-                    const Length2D groundAnchorA, const Length2D groundAnchorB,
-                    const Length2D anchorA, const Length2D anchorB);
-
-    PulleyJointDef& UseRatio(Real v) noexcept
-    {
-        ratio = v;
-        return *this;
-    }
-
-    /// The first ground anchor in world coordinates. This point never moves.
-    Length2D groundAnchorA = Length2D{Real(-1) * Meter, Real(1) * Meter};
-
-    /// The second ground anchor in world coordinates. This point never moves.
-    Length2D groundAnchorB = Length2D{Real(1) * Meter, Real(1) * Meter};
-
-    /// The local anchor point relative to bodyA's origin.
-    Length2D localAnchorA = Length2D{Real(-1) * Meter, Real(0) * Meter};
-
-    /// The local anchor point relative to bodyB's origin.
-    Length2D localAnchorB = Length2D{Real(1) * Meter, Real(0) * Meter};
-
-    /// The a reference length for the segment attached to bodyA.
-    Length lengthA = Length{0};
-
-    /// The a reference length for the segment attached to bodyB.
-    Length lengthB = Length{0};
-
-    /// The pulley ratio, used to simulate a block-and-tackle.
-    Real ratio = 1;
-};
 
 /// @brief Pulley joint.
 /// @details The pulley joint is connected to two bodies and two fixed ground points.
@@ -177,8 +135,6 @@ Length GetCurrentLengthA(const PulleyJoint& joint);
 
 /// Get the current length of the segment attached to bodyB.
 Length GetCurrentLengthB(const PulleyJoint& joint);
-
-PulleyJointDef GetPulleyJointDef(const PulleyJoint& joint) noexcept;
 
 } // namespace playrho
 
