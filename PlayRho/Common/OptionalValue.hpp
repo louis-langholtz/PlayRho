11f88fdc35661d679e07b41ee491e4d3bf0f0263
louis-langholtz <lou_langholtz@me.com>
2017-08-18 13:39:32 -0600

    For issue #42.
    Adds an OptionalValue template class to provide the C++17 std::optional functionality in C++14.

---
+++ PlayRho/Common/OptionalValue.hpp
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#ifndef OptionalValue_hpp
+#define OptionalValue_hpp
+
+#include <cassert>
+
+namespace playrho {
+    
+    template<typename T>
+    class OptionalValue
+    {
+    public:
+        using value_type = T;
+        
+        constexpr OptionalValue() noexcept = default;
+        constexpr OptionalValue(const OptionalValue& other) = default;
+        constexpr explicit OptionalValue(const T v);
+
+        constexpr const T& operator*() const;
+        constexpr T& operator*();
+
+        constexpr explicit operator bool() const noexcept;
+        constexpr bool has_value() const noexcept;
+        
+        OptionalValue& operator=(const OptionalValue& other) = default;
+        OptionalValue& operator=(const T v);
+
+        constexpr T& value();
+        constexpr const T& value() const;
+        
+        constexpr T value_or(const T& alt) const;
+        
+    private:
+        value_type m_value = value_type{};
+        bool m_set = false;
+    };
+    
+    template<typename T>
+    constexpr OptionalValue<T>::OptionalValue(const T v): m_value{v}, m_set{true} {}
+
+    template<typename T>
+    constexpr bool OptionalValue<T>::has_value() const noexcept
+    {
+        return m_set;
+    }
+    
+    template<typename T>
+    constexpr OptionalValue<T>::operator bool() const noexcept
+    {
+        return m_set;
+    }
+    
+    template<typename T>
+    OptionalValue<T>& OptionalValue<T>::operator=(const T v)
+    {
+        m_value = v;
+        m_set = true;
+        return *this;
+    }
+
+    template<typename T>
+    constexpr const T& OptionalValue<T>::operator*() const
+    {
+        assert(m_set);
+        return m_value;
+    }
+    
+    template<typename T>
+    constexpr T& OptionalValue<T>::operator*()
+    {
+        assert(m_set);
+        return m_value;
+    }
+
+    template<typename T>
+    constexpr T& OptionalValue<T>::value()
+    {
+        return m_value;
+    }
+    
+    template<typename T>
+    constexpr const T& OptionalValue<T>::value() const
+    {
+        return m_value;
+    }
+
+    template<typename T>
+    constexpr T OptionalValue<T>::value_or(const T& alt) const
+    {
+        return m_set? m_value: alt;
+    }
+    
+    template <typename T>
+    using Optional = OptionalValue<T>;
+    
+} // namespace playrho
+
+#endif /* OptionalValue_hpp */
---
+++ UnitTests/OptionalValue.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2017 Louis Langholtz https://github.com/louis-langholtz/PlayRho
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include "gtest/gtest.h"
+#include <PlayRho/Common/OptionalValue.hpp>
+
+using namespace playrho;
+
+TEST(OptionalValue, DefaultConstruction)
+{
+    OptionalValue<int> foo;
+    EXPECT_FALSE(foo.has_value());
+    EXPECT_FALSE(static_cast<bool>(foo));
+    EXPECT_FALSE(bool(foo));
+}
+
+TEST(OptionalValue, ZeroInitialization)
+{
+    OptionalValue<int> foo{};
+    EXPECT_FALSE(foo.has_value());
+    EXPECT_FALSE(static_cast<bool>(foo));
+    EXPECT_FALSE(bool(foo));
+}
+
+TEST(OptionalValue, Initialization)
+{
+    OptionalValue<int> foo{5};
+    EXPECT_TRUE(foo.has_value());
+    EXPECT_TRUE(static_cast<bool>(foo));
+    EXPECT_TRUE(bool(foo));
+    EXPECT_EQ(*foo, 5);
+}
+
+TEST(OptionalValue, AssignmentOperator)
+{
+    OptionalValue<int> foo{};
+    ASSERT_FALSE(foo.has_value());
+    
+    foo = 2;
+    EXPECT_TRUE(foo.has_value());
+    EXPECT_TRUE(bool(foo));
+    EXPECT_EQ(*foo, 2);
+    EXPECT_EQ(foo.value(), 2);
+    
+    foo = 0;
+    EXPECT_TRUE(foo.has_value());
+    EXPECT_TRUE(bool(foo));
+    EXPECT_EQ(*foo, 0);
+    EXPECT_EQ(foo.value(), 0);
+    
+    OptionalValue<int> boo;
+    ASSERT_FALSE(boo.has_value());
+
+    foo = boo;
+    EXPECT_FALSE(foo.has_value());
+}
+
+TEST(OptionalValue, value_or)
+{
+    OptionalValue<int> foo{};
+    ASSERT_FALSE(foo.has_value());
+
+    EXPECT_EQ(foo.value_or(66), 66);
+}
