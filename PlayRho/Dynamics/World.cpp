e959dad24297729458d38e06cf900b099e0c15ad
louis-langholtz <lou_langholtz@me.com>
2020-09-01 13:22:26 -0600

    Updates type name for building DO_THREADED support.

--- PlayRho/Dynamics/World.cpp
+++ PlayRho/Dynamics/World.cpp
@@ -1042,7 +1042,7 @@ RegStepStats World::SolveReg(const StepConf& conf)
     });
 
 #if defined(DO_THREADED)
-    std::vector<std::future<World::IslandSolverResults>> futures;
+    std::vector<std::future<IslandStats>> futures;
     futures.reserve(remNumBodies);
 #endif
     // Build and simulate all awake islands.
