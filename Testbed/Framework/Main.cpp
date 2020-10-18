aeaa0b0ba27f41a39ff5e1e267291ecb2cfe975a
louis-langholtz <lou_langholtz@me.com>
2020-10-18 16:28:40 -0600

    Removes unnecessarily repeated call to glfwInit.

--- Testbed/Framework/Main.cpp
+++ Testbed/Framework/Main.cpp
@@ -2366,7 +2366,6 @@ int main()
     std::sprintf(title, "PlayRho Testbed Version %d.%d.%d",
                  buildVersion.major, buildVersion.minor, buildVersion.revision);
 
-    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
