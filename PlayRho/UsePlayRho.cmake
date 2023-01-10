4933b021dee8ea12eb72cebb5287b9ea1dd462ba
louis-langholtz <louis-langholtz@users.noreply.github.com>
2023-01-10 15:14:06 -0700

    Various tasks for improving PlayRho installation and consumption via cmake
    - Drops the UsePlayRho.cmake file for a better mechanism
    - Drops non-relocatable variables in favor of new mechanism
    - Configures PlayRhoConfig.cmake.in with a package helper
    - Removes no longer supported nor necessary property settings
    - Removes no longer used UsePlayRho.cmake from file
    
    (cherry picked from commit ce2b95fae5f394792fa1703e2383fd89a8690725)

--- PlayRho/UsePlayRho.cmake
+++
@@ -1,8 +0,0 @@
-#                                               -*- cmake -*-
-#
-#  UsePlayRho.cmake
-#
-
-add_definitions     ( ${PLAYRHO_DEFINITIONS} )
-include_directories ( ${PLAYRHO_INCLUDE_DIRS} )
-link_directories    ( ${PLAYRHO_LIBRARY_DIRS} )
