8cc1d89da7de599f9d7706e1a964f682fdc5f160
Louis Langholtz <louis-langholtz@users.noreply.github.com>
2023-02-06 10:17:56 -0700

    Updates unit tests that were failing on Apple M2 Silicon
    Fix for issue #436.
    
    (cherry picked from commit b579ca4985ee81248289fff9a7876322b660bb59)

--- UnitTests/functional.cpp
+++ UnitTests/functional.cpp
@@ -22,20 +22,14 @@
 TEST(functional, FunctionSize)
 {
 #ifdef __APPLE__
+#if defined(__arm64__)
+    EXPECT_EQ(sizeof(std::function<int()>), std::size_t(32));
+#else
     EXPECT_EQ(sizeof(std::function<int()>), std::size_t(48));
-    EXPECT_EQ(sizeof(std::function<void(int)>), std::size_t(48));
-    EXPECT_EQ(sizeof(std::function<float(int)>), std::size_t(48));
-    EXPECT_EQ(sizeof(std::function<int(float)>), std::size_t(48));
-    EXPECT_EQ(sizeof(std::function<int(double)>), std::size_t(48));
-    EXPECT_EQ(sizeof(std::function<int(double, long, void*)>), std::size_t(48));
+#endif
 #endif
 #ifdef __linux__
     EXPECT_EQ(sizeof(std::function<int()>), std::size_t(32));
-    EXPECT_EQ(sizeof(std::function<void(int)>), std::size_t(32));
-    EXPECT_EQ(sizeof(std::function<float(int)>), std::size_t(32));
-    EXPECT_EQ(sizeof(std::function<int(float)>), std::size_t(32));
-    EXPECT_EQ(sizeof(std::function<int(double)>), std::size_t(32));
-    EXPECT_EQ(sizeof(std::function<int(double, long, void*)>), std::size_t(32));
 #endif
 }
 
